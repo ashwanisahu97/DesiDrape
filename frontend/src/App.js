790044adb6127f7a1e216c127c10f1de93186a19
Ashwani Sahu <ashwani.s@honeysys.com>
2023-12-08 14:55:08 +0530

    initial setup of this e-commerce is done

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Welcome to this coding world</h1>
+        <Navbar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./navbar.css"
+const Navbar = () => {
+  return (
+    <div className='navbarMainContainer'>Navbar</div>
+  )
+}
+
+export default Navbar
