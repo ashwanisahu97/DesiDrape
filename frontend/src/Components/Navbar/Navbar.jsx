1964cfb857df7a98aa1a15309aaf7d2b2fac2018
Ashwani Sahu <ashwani.s@honeysys.com>
2024-01-02 10:09:33 +0530

    added navbar file in the project

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,59 @@
-import React from 'react';
+import React from "react";
 import "./navbar.css";
 import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
 const Navbar = () => {
   let Container = styled.div`
-  height:60px;
-  border:2px solid green;
+    height: 60px;
+    border: 2px solid green;
   `;
   const Wrapper = styled.div`
-  padding:10px 20px;
-  display:flex;
-  justify-content:space-between;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
   `;
-  const Left = styled.div``;
-  const Center = styled.div``;
-  const Right = styled.div``;
+  const Left = styled.div`
+    border: 1px solid gray;
+    flex: 1;
+  `;
+  const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+  `;
+  const SearchContainer = styled.div`
+    border: 1px solid gray;
+  `;
+  const Input = styled.input``;
+  const Center = styled.div`
+    border: 1px solid gray;
+    flex: 1;
+  `;
+  const Logo = styled.h1`
+    font-weight: bold;
+  `;
+  const Right = styled.div`
+    border: 1px solid gray;
+    flex: 1;
+  `;
   return (
     <Container>
-     <Wrapper>
-    <Left>left</Left>
-    <Center>center</Center>
-    <Right>right</Right>
-     </Wrapper>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <SearchIcon />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>
+            LAMA
+          </Logo>
+        </Center>
+        <Right>right</Right>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
